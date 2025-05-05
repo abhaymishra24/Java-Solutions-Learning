d2175cb577d0ec924f6059ec93b18efbb152e7e0
Abhay Mishra <techabhay06@gmail.com>
2025-05-05 19:46:12 +0530

    Add new commit

---
+++ Java_Oops/Oops_problem1.java
@@ -0,0 +1,13 @@
+public class Oops_problem1 {
+    public static void main(String[] args) {
+        String name = "John Doe";
+        int age = 25;
+        String city = "New York";
+        String school = "ABC High School";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("City: " + city);
+        System.out.println("School: " + school);
+    }
+}
