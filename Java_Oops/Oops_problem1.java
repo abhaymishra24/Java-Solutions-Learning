402e0e47f4d28827382683f3ee18c299914e2dd4
Abhay Mishra <techabhay06@gmail.com>
2025-05-05 19:48:10 +0530

    Add new commit

--- Java_Oops/Oops_problem1.java
+++ Java_Oops/Oops_problem1.java
@@ -1,3 +1,6 @@
+
+// Oops code to print the name, age, city, and school of a person - 
+
 public class Oops_problem1 {
     public static void main(String[] args) {
         String name = "John Doe";
