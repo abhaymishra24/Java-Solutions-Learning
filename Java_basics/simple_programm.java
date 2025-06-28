db713f9a64849582a1de338d6d5c74685e2b2f93
Abhay Mishra <techabhay06@gmail.com>
2025-06-28 14:07:50 +0530

    Add enw commit

---
+++ Java_basics/simple_programm.java
@@ -0,0 +1,10 @@
+
+public class simple_programm {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello, I am Abhay who try to learn java.");
+        System.out.println("Java is a programming language that is used to create applications and software.");
+
+    }
+}
